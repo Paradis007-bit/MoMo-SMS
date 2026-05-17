2157ccddf9a26fd59d60a56252b85d2b4bbb1940
hirwapadis@gmail.com <c.hirwa2@alustudent.com>
2026-05-17 08:40:51 +0200

    Split database_setup.sql into separate table files

---
+++ backend/database/database_setup/init.sql
@@ -0,0 +1,11 @@
+-- this runs all the files in the database_setup folder to create the database and tables, and insert the initial data.
+
+CREATE DATABASE IF NOT EXISTS momo_sms;
+USE momo_sms;
+
+SOURCE database_setup/roles.sql;
+SOURCE database_setup/users.sql;
+SOURCE database_setup/transaction_category.sql;
+SOURCE database_setup/transactions.sql;
+SOURCE database_setup/transaction_participants.sql;
+SOURCE database_setup/system_logs.sql;
