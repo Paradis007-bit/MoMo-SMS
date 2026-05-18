236372fdcda9482ff56d72c099c7213840b7d558
Paradis007-bit <c.hirwa2@alustudent.com>
2026-05-18 09:55:23 +0200

    Updated transaction_category.sql
    sample data to transaction categories table

--- backend/database/database_setup/transaction_category.sql
+++ backend/database/database_setup/transaction_category.sql
@@ -3,3 +3,10 @@ CREATE TABLE Transaction_Categories (
     category_name VARCHAR(50)  NOT NULL,
     description   VARCHAR(200)
 );
+
+INSERT INTO Transaction_Categories(category_name,description) VALUES
+('Incoming Money', 'Money received from another user'),
+('Transfer', 'Money sent to another user'),
+('Payment', 'Payment made to a merchant'),
+('Airtime', 'Airtime purchase for a phone number'),
+('Withdrawal', 'Cash withdrawn at a MoMo agent');
