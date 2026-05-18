9da3c1adbb29fe63d377609b67d74606e3d5d3f2
Paradis007-bit <c.hirwa2@alustudent.com>
2026-05-18 09:57:46 +0200

    Update users.sql and added foreign keys  and role_id

--- backend/database/database_setup/users.sql
+++ backend/database/database_setup/users.sql
@@ -2,13 +2,15 @@ CREATE TABLE users (
     user_id      INT AUTO_INCREMENT PRIMARY KEY,
     phone_number VARCHAR(15)  NOT NULL UNIQUE,
     full_name    VARCHAR(100),
-    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(role_id),
+    REFERENCES roles(role_id)
 );
 
 -- Sample data
-INSERT INTO users (phone_number, full_name) VALUES 
-('250790772377', 'Samuel Carter'),
-('250791666066', 'Alex Doe'),
-('250788999999', 'Robert Brown'),
-('250788994599', 'Linda Green'),
-('250791662166', 'Jane Smith');
+INSERT INTO users (phone_number, full_name,role_id) VALUES 
+('250790772377', 'Samuel Carter',2),
+('250791666066', 'Alex Doe',2),
+('250788999999', 'Robert Brown',2),
+('250788994599', 'Linda Green',3),
+('250791662166', 'Jane Smith',3);
